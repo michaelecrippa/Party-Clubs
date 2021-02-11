0066c3764cb1e922fc997afc3f730ff16e75be3a
Michael Crippa <michael_crippa@abv.bg>
2021-02-11 10:10:24 +0200

    changed const to constexpr

--- Resources/Constants.h
+++ Resources/Constants.h
@@ -1,19 +1,19 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-const double GUARD_FEE = 20.0;
-const int LEGAL_AGE = 18;
+constexpr double GUARD_FEE = 20.0;
+constexpr int LEGAL_AGE = 18;
 
-const int FOLK_MAX_USERS = 70;
-const double FOLK_MIN_WHISKY_PRICE = 35.0;
-const double FOLK_MIN_VODKA_PRICE = 20.0;
+constexpr int FOLK_MAX_USERS = 70;
+constexpr double FOLK_MIN_WHISKY_PRICE = 35.0;
+constexpr double FOLK_MIN_VODKA_PRICE = 20.0;
 
-const double HOUSE_MIN_WHISKY_PRICE = 30.0;
-const double HOUSE_MIN_VODKA_PRICE = 40.0;
+constexpr double HOUSE_MIN_WHISKY_PRICE = 30.0;
+constexpr double HOUSE_MIN_VODKA_PRICE = 40.0;
 
-const int ROCK_MAX_USERS = 30;
-const double ROCK_MIN_WHISKY_PRICE = 30.0;
-const double ROCK_MIN_VODKA_PRICE = 40.0;
+constexpr int ROCK_MAX_USERS = 30;
+constexpr double ROCK_MIN_WHISKY_PRICE = 30.0;
+constexpr double ROCK_MIN_VODKA_PRICE = 40.0;
 
 #endif // !_CONSTANTS_H_
 
